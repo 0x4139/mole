9c89974b3540831169d113f418e0bff0c8b7e312
Vali Malinoiu <vali@Valis-MacBook-Pro.local>
2015-08-18 13:54:01 +0300

    Initial commit

---
+++ mole.go
@@ -0,0 +1,17 @@
+package main
+import "flag"
+import "fmt"
+import "github.com/elazarl/goproxy"
+import "log"
+import "net/http"
+
+func main()  {
+  proxyIP := flag.String("ip", "127.0.0.1", "ip that the proxy server should use")
+  proxyPort := flag.String("port", "5445", "the port that the proxy server should use")
+  proxyVerbose:= flag.Bool("verbose","true","if the proxy server should be verbose or not")
+  flag.Parse()
+  proxy := goproxy.NewProxyHttpServer()
+  proxy.Verbose = true
+  fmt.Println("Starting server on:",fmt.Sprint(*proxyIP,":",*proxyPort))
+  log.Fatal(http.ListenAndServe(fmt.Sprint(*proxyIP,":",*proxyPort), proxy))
+}
